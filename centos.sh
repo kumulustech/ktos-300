ca6fc60c83d01e0e41b6e2cf8140a3f6d05bde7f
Robert Starmer <rstarmer@gmail.com>
2016-09-20 12:08:44 -0400

    Put the centos model into a script

---
+++ centos.sh
@@ -0,0 +1,83 @@
+setenforce 0
+sed -i "s/^\s*SELINUX=.*/SELINUX=disabled/g" /etc/selinux/config
+
+yum -y install epel-release centos-release-openstack-mitaka
+
+yum -y install \
+    vim \
+    net-tools \
+    python-pip \
+    python-devel \
+    python-docker-py \
+    python-openstackclient \
+    python-neutronclient \
+    libffi-devel \
+    openssl-devel \
+    gcc \
+    make \
+    ntp \
+    docker
+
+pip install -U pip
+mkdir -p /etc/systemd/system/docker.service.d
+tee /etc/systemd/system/docker.service.d/kolla.conf <<-EOF
+[Service]
+MountFlags=shared
+EOF
+
+systemctl daemon-reload
+systemctl enable docker
+systemctl enable ntpd.service
+systemctl restart docker
+systemctl restart ntpd.service
+
+systemctl stop libvirtd.service
+systemctl disable libvirtd.service
+
+pip install ansible==1.9.6
+pip install kolla
+
+cp -r /usr/share/kolla/etc_examples/kolla /etc/
+
+NETWORK_INTERFACE="eth1"
+NEUTRON_INTERFACE="eth2"
+GLOBALS_FILE="/etc/kolla/globals.yml"
+ADDRESS="$(ip -4 addr show ${NETWORK_INTERFACE} | grep "inet" | awk '{print $2}' | cut -d/ -f1)"
+BASE="$(echo $ADDRESS | cut -d. -f 1,2,3)"
+VIP=$(echo "${BASE}.254")
+
+sed -i "s/^kolla_internal_vip_address:.*/kolla_internal_vip_address: \\"${VIP}\\"/g" ${GLOBALS_FILE}
+sed -i "s/^network_interface:.*/network_interface: \\"${NETWORK_INTERFACE}\\"/g" ${GLOBALS_FILE}
+sed -i "s/^neutron_external_interface:.*/neutron_external_interface: \\"${NEUTRON_INTERFACE}\\"/g" ${GLOBALS_FILE}
+ sed -i "s/^docker_registry:.*/docker_registry: '10.133.210.52:4000'" ${GLOBALS_FILE}
+ sed -i "s/^docker_registry:.*/docker_registry: 'kolla.opsits.com:4000'" ${GLOBALS_FILE}
+echo "${ADDRESS} localhost" >> /etc/hosts
+
+mkdir -p /etc/kolla/config/nova/
+tee > /etc/kolla/config/nova/nova-compute.conf <<-EOF
+[libvirt]
+virt_type=qemu
+EOF
+
+kolla-genpwd
+sed -i -e "s/^keystone_admin_password:.*/keystone_admin_password: admin1/" /etc/kolla/passwords.yml
+kolla-ansible prechecks
+kolla-ansible pull
+kolla-ansible deploy
+
+tee > /root/open.rc <<EOF
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=$(cat /etc/kolla/passwords.yml | grep "keystone_admin_password" | awk '{print $2}')
+export OS_AUTH_URL=http://${ADDRESS}:35357/v3
+export OS_IDENTITY_API_VERSION=3
+EOF
+
+bash ./import_image.sh
+
+bash ./setup_network.sh
+
+echo "Login using http://${ADDRESS} with default as domain,  admin as username, and $(cat /etc/kolla/passwords.yml | grep "keystone_admin_password" | awk '{print $2}') as password"
